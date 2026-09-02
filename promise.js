40e3b08b114700e0c9efed420a34c4915be4eb2b
mohd-ashraf24 <93ashraf717@gmail.com>
2026-09-02 14:41:49 +0530

    promise

---
+++ promise.js
@@ -0,0 +1,22 @@
+function f1(){
+    return new Promise((resolve , reject) =>{
+        setTimeout(() =>{
+        console.log("hello");
+        resolve();
+    } , 4000);
+    })
+}
+
+function f2(){
+    return new Promise((resolve , reject) =>{
+        setTimeout(() =>{
+            console.log("abes");
+            resolve();
+    } , 1000)
+    })
+}
+
+f1().then(f2)
+    .catch((err) =>{
+        console.log("error" , err);
+    })
